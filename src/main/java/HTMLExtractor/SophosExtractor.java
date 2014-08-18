85aa89e2c28b0cfd0a01caa00156bee8431b4200
euf <euf@ornl.gov>
2014-08-18 15:52:44 -0400

    Sophos extractor - adding remaining fields from summary page, minor bugfixes

--- src/main/java/HTMLExtractor/SophosExtractor.java
+++ src/main/java/HTMLExtractor/SophosExtractor.java
@@ -1,9 +1,13 @@
 package HTMLExtractor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class SophosExtractor extends HTMLExtractor{
@@ -36,14 +40,14 @@ public class SophosExtractor extends HTMLExtractor{
 		//process the "summary" page
 		Document doc = Jsoup.parse(summary);
 		Element content = doc.getElementsByClass("tertiaryBump").first();
-		if(debug){
+		if(verboseDebug){
 			System.out.println(content.html());
 			System.out.println("=========");
 		}
 		
 		//get the title, set up name & other known fields
 		Element titleDiv = content.getElementsByClass("marqTitle").first();
-		if(debug){
+		if(verboseDebug){
 			System.out.println(titleDiv.html());
 			System.out.println("=========");
 		}
@@ -63,15 +67,65 @@ public class SophosExtractor extends HTMLExtractor{
 		String category = rowOne.child(1).text();
 		vertex.put("category", category);
 		String addedDate = rowOne.child(3).text();
-		vertex.put("addedDate", convertTimestamp(addedDate));
+		if(!addedDate.equals(""))//some don't list dates, not sure why
+			vertex.put("addedDate", convertTimestamp(addedDate));
 		Element rowTwo = titleDiv.getElementsByTag("tr").get(1);
 		String type = rowTwo.child(1).text();
 		vertex.put("type", type);
 		String modifiedDate = rowTwo.child(3).text();
-		vertex.put("modifiedDate", convertTimestamp(modifiedDate));
+		if(!modifiedDate.equals(""))
+			vertex.put("modifiedDate", convertTimestamp(modifiedDate));
+		Element rowThree = titleDiv.getElementsByTag("tr").get(2);
+		String prevalence = rowThree.child(1).getElementsByTag("img").first().attr("alt");
+		vertex.put("prevalence", prevalence); //TODO: convert labels into int levels, or similar?
+		if(debug){
+			System.out.println("Prevalence: " + prevalence);
+			System.out.println("=========");
+		}
 		
-		Element secondaryDiv = doc.getElementById("secondaryContent");
-		//TODO handle secondary div
+		//handle secondary div
+		Element secondaryDiv = doc.getElementsByClass("secondaryContent").first();
+		if(verboseDebug){
+			System.out.println(secondaryDiv.html());
+			System.out.println("=========");
+		}
+		Elements aliasItems = secondaryDiv.getElementsByClass("aliases");
+		if(aliasItems != null && aliasItems.size() > 0){ //some don't have any listed.
+			aliasItems = aliasItems.first().children();
+			if(verboseDebug){
+				System.out.println(aliasItems.outerHtml());
+				System.out.println("=========");
+			}
+			List<String> aliasList = new ArrayList<String>();
+			for(int i=0; i<aliasItems.size(); i++){
+				aliasList.add(aliasItems.get(i).text());
+			}
+			//TODO: how best to handle aliases in the long term?
+			vertex.put("aliases", aliasList);
+			if(debug){
+				System.out.println("Found " + aliasList.size() + " items in aliasList:");
+				for(int i=0; i<aliasList.size(); i++){
+					System.out.println(aliasList.get(i));
+				}
+				System.out.println("=========");
+			}
+		}
+		Elements h3s = secondaryDiv.getElementsByTag("h3");
+		Element affectedHeading = null;
+		for(int i=0; i<h3s.size(); i++){
+			if(h3s.get(i).text().equals("Affected Operating Systems")){
+				affectedHeading = h3s.get(i);
+				break;
+			}
+		}
+		String platformName = affectedHeading.nextElementSibling().getElementsByTag("img").first().attr("alt");
+		vertex.put("platform", platformName);
+		if(debug){
+			if(affectedHeading != null){
+				System.out.println("Platform: " + platformName);
+				System.out.println("=========");
+			}
+		}
 		
 		////////////////////////////////////
 		//process the "details" page
