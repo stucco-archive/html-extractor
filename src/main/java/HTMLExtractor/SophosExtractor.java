efc4263d35b541614d08a5d7a010186490080a22
euf <euf@ornl.gov>
2014-08-20 11:26:00 -0400

    handling sophos details page

--- src/main/java/HTMLExtractor/SophosExtractor.java
+++ src/main/java/HTMLExtractor/SophosExtractor.java
@@ -1,7 +1,11 @@
 package HTMLExtractor;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.json.*;
 import org.jsoup.Jsoup;
@@ -26,6 +30,10 @@ public class SophosExtractor extends HTMLExtractor{
 	
 	private long convertTimestamp(String time)	{ 
 		return convertTimestamp(time, "dd MMM yyyy hh:mm:ss (z)");
+	}
+	
+	private long convertShortTimestamp(String time)	{ 
+		return convertTimestamp(time, "yyyy-MM-dd");
 	}
 	
 	private JSONObject extract(String summary, String details){
@@ -67,8 +75,10 @@ public class SophosExtractor extends HTMLExtractor{
 		String category = rowOne.child(1).text();
 		vertex.put("category", category);
 		String addedDate = rowOne.child(3).text();
-		if(!addedDate.equals(""))//some don't list dates, not sure why
-			vertex.put("addedDate", convertTimestamp(addedDate));
+		if(!addedDate.equals("")){//some don't list dates, not sure why
+			vertex.put("signatureDate", convertTimestamp(addedDate));
+			vertex.put("discoveryDate", convertTimestamp(addedDate));
+		}
 		Element rowTwo = titleDiv.getElementsByTag("tr").get(1);
 		String type = rowTwo.child(1).text();
 		vertex.put("type", type);
@@ -130,9 +140,94 @@ public class SophosExtractor extends HTMLExtractor{
 		////////////////////////////////////
 		//process the "details" page
 		doc = Jsoup.parse(details);
-		content = doc.getElementById("asdf");//TODO
-		//vertex.put("description", content.text());
+		content = doc.getElementsByClass("threatDetail").first();
 		
+		//handle the "File Information" tables
+		Elements h4headings = content.getElementsByTag("h4");
+		Element curr, nextSibling;
+		Map<String,String> currTableContents;
+		TreeSet<String> size = new TreeSet<String>();
+		TreeSet<String> sha1 = new TreeSet<String>();
+		TreeSet<String> md5 = new TreeSet<String>();
+		TreeSet<String> crc32 = new TreeSet<String>();
+		TreeSet<String> filetype = new TreeSet<String>();
+		//TreeSet<String> filetype = new TreeSet<String>();
+		long firstSeen;
+		boolean runtimeAnalysisFound = false;
+		for(int i=0; i<h4headings.size(); i++){
+			curr = h4headings.get(i);
+			nextSibling = curr.nextElementSibling();
+			if(curr.text().equals("File Information") && nextSibling.tagName().equals("dl")){
+				if(verboseDebug) System.out.println("Found a file info table: \n" + nextSibling.html());
+				currTableContents = dlToMap(nextSibling); //TODO code below will NPE if this is null.  Fine while testing, should fix before using. 
+				if(debug) System.out.println("Extracted map from file info table: " + currTableContents);
+				if(currTableContents.containsKey("Size")){
+					size.add(currTableContents.get("Size"));
+				}
+				if(currTableContents.containsKey("SHA-1")){
+					sha1.add(currTableContents.get("SHA-1"));
+				}
+				if(currTableContents.containsKey("MD5")){
+					md5.add(currTableContents.get("MD5"));
+				}
+				if(currTableContents.containsKey("CRC-32")){
+					crc32.add(currTableContents.get("CRC-32"));
+				}
+				if(currTableContents.containsKey("File type")){
+					filetype.add(currTableContents.get("File type"));
+				}
+				if(currTableContents.containsKey("First seen")){
+					firstSeen = convertShortTimestamp(currTableContents.get("First seen"));
+					if(firstSeen < vertex.getLong("discoveryDate")){
+						vertex.put("discoveryDate", firstSeen);
+					}
+				}
+			}else if(curr.text().equals("Runtime Analysis")){
+				//could do this here, but it's kind of complicated, better to separate it out...
+				runtimeAnalysisFound = true;
+				if(debug) System.out.println("'Runtime Analysis' section found");
+			}else if(curr.text().equals("Other vendor detection") && nextSibling.tagName().equals("dl")){
+				currTableContents = dlToMap(nextSibling); //TODO code below will NPE if this is null.  Fine while testing, should fix before using. 
+				if(debug) System.out.println("Extracted map from 'other vendor detection table: " + currTableContents);
+				JSONArray aliasArr = vertex.optJSONArray("aliases");
+				Set<String> aliasSet = JSONArrayToSet(aliasArr);
+				Set<String> keys = currTableContents.keySet();
+				Iterator<String> keysIter = keys.iterator();
+				while(keysIter.hasNext()){
+					aliasSet.add(currTableContents.get(keysIter.next()));
+				}
+				if(debug) System.out.println("now know aliases: " + aliasSet);
+				vertex.put("aliases", aliasSet);
+			}else{
+				if(debug) System.out.println("Unexpected H4 Found: " + curr.text());
+			}
+			
+		}
+		if(debug) System.out.println("=========");
+
+		//use what you've learned.
+		//if(size != null && size.size() > 0){} //not keeping this one
+		if(sha1 != null && sha1.size() > 0){
+			vertex.put("knownSha1Hashes", sha1);
+		}
+		if(md5 != null && md5.size() > 0){
+			vertex.put("knownMD5Hashes", md5);
+		}
+		//if(crc32 != null && crc32.size() > 0){} //not keeping this one
+		if(filetype != null && filetype.size() > 0){
+			vertex.put("knownFileTypes", filetype);
+		}
+		
+		//TODO: handle the "Runtime Analysis" sections...
+		if(runtimeAnalysisFound){
+			if(debug){
+				if(debug) System.out.println("'Runtime Analysis' section found");
+				if(debug) System.out.println("=========");
+			}
+		}
+		
+		//TODO put some remaining free text in desc?
+		//vertex.put("description", content.text());
 	    
 		vertices.put(vertex);
 		
