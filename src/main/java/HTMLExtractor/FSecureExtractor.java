3c7e572e784fbc9349fbc8030f8f167122427276
euf <euf@ornl.gov>
2014-11-18 14:39:23 -0500

    update f-secure extractor, for new page format

--- src/main/java/HTMLExtractor/FSecureExtractor.java
+++ src/main/java/HTMLExtractor/FSecureExtractor.java
@@ -63,71 +63,59 @@ public class FSecureExtractor extends HTMLExtractor{
 		
 		////////////////////////////////////
 		//get the title, set up name & other known fields
-		Element titleDiv = doc.getElementById("title-page-alt");
-		String vertexName = titleDiv.text();
+		String vertexName = doc.getElementsByTag("title").first().text().replaceAll("\u200b", "").replaceAll("\\:\\?",":");
+		
+		
 		logger.info("Name: {}", vertexName);
 		vertex.put("name", vertexName);
 		vertex.put("_id", vertexName);
 		vertex.put("_type", "vertex");
 		vertex.put("vertexType", "malware");
 		vertex.put("source", "F-Secure");
-		
+
 		/////////////////////////////
 		//parse the box at the top
-		Element boxDiv = doc.select("div.box-rounded.br-blue").first().select("div.f-content").first();
-		logger.debug(boxDiv.html());
-		Element aliasDiv = boxDiv.select("div").first().select("div").get(2);
-		logger.debug(aliasDiv.html());
-		String[] aliasList = aliasDiv.text().split(" ");
+		Element detailsTable = doc.getElementsByClass("details-table").first();
+		String[][] cells = getCells(detailsTable.getElementsByTag("tr"));
+		
+		logger.debug("table contains: {}", new JSONArray(cells));
+		
+		String aliases = cells[0][1];
+		String category = cells[1][1];
+		String type = cells[2][1];
+		String platform = cells[3][1];
+		
+		String[] aliasList = aliases.split(", ");
+		logger.debug("Found {} items in aliasList:", aliasList.length);
+		for(int i=0; i<aliasList.length; i++){
+			logger.debug(aliasList[i]);
+		}
 		Set<String> aliasSet = new TreeSet<String>();
 		for(String alias : aliasList){
-			aliasSet.add(alias);
+			aliasSet.add(alias.replaceAll("\u200b", "").replaceAll("\\:\\?",":"));
 		}
 		aliasSet.add(vertexName);
+		JSONArray aliasesResult = new JSONArray(aliasSet);
+		logger.info("final alias list is: {}", aliasesResult);
+		vertex.put("aliases", aliasesResult);
 		//TODO: how best to handle aliases in the long term?
-		vertex.put("aliases", new JSONArray(aliasSet));
-		logger.info("Found {} items in aliasList:", aliasList.length);
-		for(int i=0; i<aliasList.length; i++){
-			logger.info(aliasList[i]);
-		}
 		
-		//I think using divs as a table might actually be worse than using a table as divs...
-		Element leftCatsDiv = boxDiv.select("div").first().select("div").get(3);
-		logger.debug(leftCatsDiv.html());
-		String[] leftCatsList = leftCatsDiv.text().replace(":","").split(" ");
-		logger.info("Found {} items in leftCatsList:", leftCatsList.length);
-		for(int i=0; i<leftCatsList.length; i++){
-			logger.info(leftCatsList[i]);
-		}
+		//keeping both fields as "malwareType", because they aren't used consistently
+		JSONArray types = new JSONArray();
+		types.put(category);
+		types.put(type);
+		vertex.put("malwareType", types);
+		vertex.put("platform", platform);
+		
 
-		Element rightCatsDiv = boxDiv.select("div").first().select("div").get(4);
-		logger.debug(rightCatsDiv.html());
-		String[] rightCatsList = rightCatsDiv.text().split(" ");
-		logger.info("Found {} items in rightCatsList:", rightCatsList.length);
-		for(int i=0; i<rightCatsList.length; i++){
-			logger.info(rightCatsList[i]);
-		}
-		//make sure this is what you expected, then store fields.
-		if(leftCatsList.length == 3 &&
-			leftCatsList[0].equalsIgnoreCase("Category") && 
-			leftCatsList[1].equalsIgnoreCase("Type") &&
-			leftCatsList[2].equalsIgnoreCase("Platform"))
-		{
-			//keeping both fields as "malwareType", because they aren't used consistently
-			JSONArray types = new JSONArray();
-			types.put(rightCatsList[0]);
-			types.put(rightCatsList[1]);
-			vertex.put("malwareType", types);
-			vertex.put("platform", rightCatsList[2]);
-		}
-		
 		/////////////////////////
 		//parse remaining page contents
-		Element contentDiv = doc.select("div.content-protection").first().select("div.f-content").first();
+		Element contentDiv = doc.select("div#maincontent").first().select("div.row").first().select("div").first();
 		logger.debug(contentDiv.html());
 		Elements contents = contentDiv.children().first().children();
 		Element curr, prev;
 		removeBRs(contents);
+		removeHRs(contents);
 		fixSectionHeaders(contents);
 		logger.debug(contents.outerHtml());
 		//System.out.println("contents size is now: " + contents.size());
@@ -153,23 +141,23 @@ public class FSecureExtractor extends HTMLExtractor{
 				continue;
 			}
 			//pull out expected fields, based on headers...
-			if(curr.tagName().equals("p") && prev.tagName().equals("h3") && prev.text().equals("Technical Details")){
+			if(curr.tagName().equals("p") && prev.tagName().equals("h2") && prev.text().equals("Technical Details")){
 				vertex.put("details", curr.text());
 				contents.remove(i);
 				contents.remove(i-1);
 				i--;
 				continue;
 			}
-			if(curr.tagName().equals("p") && prev.tagName().equals("h3") && prev.text().equals("Summary")){
+			if(curr.tagName().equals("p") && prev.tagName().equals("h2") && prev.text().equals("Summary")){
 				vertex.put("overview", curr.text());
 				contents.remove(i);
 				contents.remove(i-1);
 				i--;
 				continue;
 			}
-			if(curr.tagName().equals("p") && prev.tagName().equals("h4") && prev.text().equals("Automatic Disinfection")){
+			if(curr.tagName().equals("p") && prev.tagName().equals("h5") && prev.text().equals("Automatic action")){
 				String removalMessage = curr.text();
-				if(removalMessage.startsWith("Allow F-Secure Anti-Virus to disinfect the relevant files.")){
+				if(removalMessage.startsWith("Once detected, the F-Secure security product will automatically disinfect the suspect file")){
 					vertex.put("removal", "F-Secure");
 				}else{
 					vertex.put("removal", "F-Secure: " + removalMessage);
@@ -189,6 +177,13 @@ public class FSecureExtractor extends HTMLExtractor{
 			}
 			if(curr.tagName().equals("p") && prev.tagName().equals("h4") && prev.text().equals("Behavior")){
 				vertex.put("behavior", curr.text());
+				contents.remove(i);
+				contents.remove(i-1);
+				i -= 1;
+				continue;
+			}
+			if(curr.tagName().equals("p") && (prev.tagName().equals("h5") || prev.tagName().equals("h4")) && prev.text().equals("More")){
+				//Don't put contents anywhere for these, just ignore.
 				contents.remove(i);
 				contents.remove(i-1);
 				i -= 1;
