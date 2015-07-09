fed6deb4a73c68944504ef836964d4da02ddc54b
MariaVincent <mvincen2@vols.utk.edu>
2015-07-09 19:10:55 -0400

    modified test cases

--- src/test/java/HTMLExtractor/BugtraqToStixExtractorTest.java
+++ src/test/java/HTMLExtractor/BugtraqToStixExtractorTest.java
@@ -14,7 +14,10 @@ import org.json.JSONObject;
 import org.junit.Test;
 
 import org.mitre.stix.stix_1.STIXPackage;
-
+import org.mitre.stix.common_1.ExploitTargetsType;
+import org.mitre.stix.exploittarget_1.ExploitTarget;
+import org.mitre.stix.exploittarget_1.VulnerabilityType;
+						
 import static org.junit.Assert.*;
 
 /**
@@ -87,38 +90,82 @@ public class BugtraqToStixExtractorTest {
 			
 			//TODO maybe add a BugtraqExtractor(Map)?
 			BugtraqToStixExtractor bugtraqExt = new BugtraqToStixExtractor(info, discussion, exploit, solution, references);
-			STIXPackage stixPackage = bugtraqExt.getGraph();
-		    
-		    //System.out.println(obj.toString(2));
+			STIXPackage receivedPackage = bugtraqExt.getStixPackage();
+			
+		//	System.out.println(receivedPackage.toXMLString(true));		    
 
-	//	    JSONArray verts = obj.getJSONArray("vertices");
-	//	    JSONArray edges = obj.getJSONArray("edges");
-						    
-		    String expectedVerts = "{"+
-		    		  "  'accessVector': 'LOCAL'," +
-		    		  "  'Credit': 'This vulnerability was discovered by Richard Silverman &lt;slade@shore.net&gt;, and first announced by SSH Communications Security in an advisory posted to Bugtraq on January 16, 2001.'," +
-		    		  "  'class': 'Design Error'," +
-		    		  "  'CVE': ''," +
-		    		  "  'solution': 'Solution: Patches available: SSH Communications Security SSH 1.2.27 SSH Communications SSH1 patch-ssh-1.2.30-secure.rpc http://www.ssh.com/products/ssh/patches/patch-ssh-1.2.30-secure.rpc SSH Communications Security SSH 1.2.28 SSH Communications SSH1 patch-ssh-1.2.30-secure.rpc http://www.ssh.com/products/ssh/patches/patch-ssh-1.2.30-secure.rpc SSH Communications Security SSH 1.2.29 SSH Communications SSH1 patch-ssh-1.2.30-secure.rpc http://www.ssh.com/products/ssh/patches/patch-ssh-1.2.30-secure.rpc SSH Communications Security SSH 1.2.30 SSH Communications SSH1 patch-ssh-1.2.30-secure.rpc http://www.ssh.com/products/ssh/patches/patch-ssh-1.2.30-secure.rpc'," +
-		    		  "  'exploit': 'This exploit was contributed by Richard Silverman <slade@shore.net> : /data/vulnerabilities/exploits/ssh1-exploit.c'," +
-		    		  "  'modifiedDate': 979603200000," +
-		    		  "  'vertexType': 'vulnerability'," +
-		    		  "  '_type': 'vertex'," +
-		    		  "  'references': [],"+
-		    		  "  '_id': 'Bugtraq_2222'," +
-		    		  "  'source': 'Bugtraq'," +
-		    		  "  'shortDescription': 'SSH Secure-RPC Weak Encrypted Authentication Vulnerability'," +
-		    		  "  'description': 'SSH Secure-RPC Weak Encrypted Authentication Vulnerability SSH is a package designed to encrypt traffic between two end points using the IETF specified SSH protocol. The SSH1 package is distributed and maintained by SSH Communications Security. A problem exists which could allow the discovery of the secret key used to encrypt traffic on the local host. When using SUN-DES-1 to share keys with other hosts on the network to facilitate secure communication via protocols such as NFS and NIS+, the keys are shared between hosts using the private key of the user and a cryptographic algorithm to secure the contents of the key, which is stored on the NIS+ primary. The problem occurs when the key is encrypted with the SUN-DES-1 magic phrase prior to having done a keylogin (the keyserv does not have the users DH private key). A design flaw in the software that shares the key with the NIS+ master will inconsistently return the correct value for an attempted keyshare that has failed. A step in the private key encryption process is skipped, and the users private key is then encrypted only with the public key of the target server and the SUN-DES-1 magic phrase, a phrase that is guessable due to the way it is generated. A user from the same host can then execute a function that returns another users magic phrase, and use this to decrypt the private key of the victim. This makes it possible for a user with malicious intent to gain knowledge of a users secret key, and decrypt sensitive traffic between two hosts, with the possibility of gaining access and elevated privileges on the hosts and/or NIS+ domain. This reportedly affects the SSH2 series of the software package.'," +
-		    		  "  'name': 'Bugtraq ID 2222'," +
-		    		  "  'Vulnerable': [" +
-		    		  "    'SSH Communications Security SSH 1.2.30'," +
-		    		  "    'SSH Communications Security SSH 1.2.29'," +
-		    		  "    'SSH Communications Security SSH 1.2.28'," +
-		    		  "    'SSH Communications Security SSH 1.2.27'" +
-		    		  "  ]," +
-		    		  "  'Not_Vulnerable': []," +
-		    		  "  'publishedDate': 979603200000," +
-			    	  "}]";
+		    String expectedVerts = 
+			    "<?xml version=\"1.0\" encoding=\"UTF-8\"?> " +
+			    "<stix:STIX_Package " +
+			    "    id=\"stucco:bugtraq-b843cbb8-4a3c-4743-8585-71534f787040\" " +
+			    "    timestamp=\"2015-07-09T21:53:22.948Z\" " +
+			    "    xmlns:ProductObj=\"http://cybox.mitre.org/objects#ProductObject-2\" " +
+			    "    xmlns:coa=\"http://stix.mitre.org/CourseOfAction-1\" " +
+			    "    xmlns:cybox=\"http://cybox.mitre.org/cybox-2\" " +
+			    "    xmlns:et=\"http://stix.mitre.org/ExploitTarget-1\" " +
+			    "    xmlns:stix=\"http://stix.mitre.org/stix-1\" " +
+			    "    xmlns:stixCommon=\"http://stix.mitre.org/common-1\" xmlns:stucco=\"stucco\"> " +
+			    "    <stix:STIX_Header> " +
+			    "        <stix:Title>Bugtraq</stix:Title> " +
+			    "    </stix:STIX_Header> " +
+			    "    <stix:Exploit_Targets> " +
+			    "        <stixCommon:Exploit_Target " +
+			    "            id=\"stucco:bugtraq-5a3fefea-8faa-4eed-adcb-2a953e6bd788\" " +
+			    "            xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:type=\"et:ExploitTargetType\"> " +
+			    "            <et:Vulnerability> " +
+			    "                <et:Description>SSH Secure-RPC Weak Encrypted Authentication Vulnerability SSH is a package designed to encrypt traffic between two end points using the IETF specified SSH protocol. The SSH1 package is distributed and maintained by SSH Communications Security. A problem exists which could allow the discovery of the secret key used to encrypt traffic on the local host. When using SUN-DES-1 to share keys with other hosts on the network to facilitate secure communication via protocols such as NFS and NIS+, the keys are shared between hosts using the private key of the user and a cryptographic algorithm to secure the contents of the key, which is stored on the NIS+ primary. The problem occurs when the key is encrypted with the SUN-DES-1 magic phrase prior to having done a keylogin (the keyserv does not have the users DH private key). A design flaw in the software that shares the key with the NIS+ master will inconsistently return the correct value for an attempted keyshare that has failed. A step in the private key encryption process is skipped, and the users private key is then encrypted only with the public key of the target server and the SUN-DES-1 magic phrase, a phrase that is guessable due to the way it is generated. A user from the same host can then execute a function that returns another users magic phrase, and use this to decrypt the private key of the victim. This makes it possible for a user with malicious intent to gain knowledge of a users secret key, and decrypt sensitive traffic between two hosts, with the possibility of gaining access and elevated privileges on the hosts and/or NIS+ domain. This reportedly affects the SSH2 series of the software package.</et:Description> " +
+			    "                <et:Short_Description>SSH Secure-RPC Weak Encrypted Authentication Vulnerability</et:Short_Description> " +
+			    "                <et:Source>Bugtraq</et:Source> " +
+			    "                <et:Discovered_DateTime>2001-01-15T19:00:00.000-05:00</et:Discovered_DateTime> " +
+			    "                <et:Affected_Software> " +
+			    "                    <et:Affected_Software> " +
+			    "                        <stixCommon:Observable> " +
+			    "                            <cybox:Object> " +
+			    "                                <cybox:Properties xsi:type=\"ProductObj:ProductObjectType\"> " +
+			    "                                    <ProductObj:Product>SSH Communications Security SSH 1.2.30</ProductObj:Product> " +
+			    "                                </cybox:Properties> " +
+			    "                            </cybox:Object> " +
+			    "                        </stixCommon:Observable> " +
+			    "                    </et:Affected_Software> " +
+			    "                    <et:Affected_Software> " +
+			    "                        <stixCommon:Observable> " +
+			    "                            <cybox:Object> " +
+			    "                                <cybox:Properties xsi:type=\"ProductObj:ProductObjectType\"> " +
+			    "                                    <ProductObj:Product>SSH Communications Security SSH 1.2.29</ProductObj:Product> " +
+			    "                                </cybox:Properties> " +
+			    "                            </cybox:Object> " +
+			    "                        </stixCommon:Observable> " +
+			    "                    </et:Affected_Software> " +
+			    "                    <et:Affected_Software> " +
+			    "                        <stixCommon:Observable> " +
+			    "                            <cybox:Object> " +
+			    "                                <cybox:Properties xsi:type=\"ProductObj:ProductObjectType\"> " +
+			    "                                    <ProductObj:Product>SSH Communications Security SSH 1.2.28</ProductObj:Product> " +
+			    "                                </cybox:Properties> " +
+			    "                            </cybox:Object> " +
+			    "                        </stixCommon:Observable> " +
+			    "                    </et:Affected_Software> " +
+			    "                    <et:Affected_Software> " +
+			    "                        <stixCommon:Observable> " +
+			    "                            <cybox:Object> " +
+			    "                                <cybox:Properties xsi:type=\"ProductObj:ProductObjectType\"> " +
+			    "                                    <ProductObj:Product>SSH Communications Security SSH 1.2.27</ProductObj:Product> " +
+			    "                                </cybox:Properties> " +
+			    "                            </cybox:Object> " +
+			    "                        </stixCommon:Observable> " +
+			    "                    </et:Affected_Software> " +
+			    "                </et:Affected_Software> " +
+			    "            </et:Vulnerability> " +
+			    "            <et:Potential_COAs> " +
+			    "                <et:Potential_COA> " +
+			    "                    <stixCommon:Course_Of_Action xsi:type=\"coa:CourseOfActionType\"> " +
+			    "                        <coa:Description>Solution: Patches available: SSH Communications Security SSH 1.2.27 SSH Communications SSH1 patch-ssh-1.2.30-secure.rpc http://www.ssh.com/products/ssh/patches/patch-ssh-1.2.30-secure.rpc SSH Communications Security SSH 1.2.28 SSH Communications SSH1 patch-ssh-1.2.30-secure.rpc http://www.ssh.com/products/ssh/patches/patch-ssh-1.2.30-secure.rpc SSH Communications Security SSH 1.2.29 SSH Communications SSH1 patch-ssh-1.2.30-secure.rpc http://www.ssh.com/products/ssh/patches/patch-ssh-1.2.30-secure.rpc SSH Communications Security SSH 1.2.30 SSH Communications SSH1 patch-ssh-1.2.30-secure.rpc http://www.ssh.com/products/ssh/patches/patch-ssh-1.2.30-secure.rpc</coa:Description> " +
+			    "                    </stixCommon:Course_Of_Action> " +
+			    "                </et:Potential_COA> " +
+			    "            </et:Potential_COAs> " +
+			    "        </stixCommon:Exploit_Target> " +
+			    "    </stix:Exploit_Targets> " +
+			    "</stix:STIX_Package> ";
 
 		    String expectedEdges = "[{" + 
 		    		  "  '_type': 'edge'," +
@@ -164,9 +211,20 @@ public class BugtraqToStixExtractorTest {
 			    	  "  '_inV': 'Bugtraq_2222',"+
 			    	  "  'inVType': 'vulnerability'"+
 		    		  "}]";
-		    
-			System.out.println(stixPackage.toXMLString(true));
-			assertTrue(bugtraqExt.validate(stixPackage));
+		    											
+			STIXPackage expectedPackage = new STIXPackage().fromXMLString(expectedVerts);
+										
+			assertTrue(bugtraqExt.validate(receivedPackage));
+		
+			ExploitTarget etReceived = (ExploitTarget)receivedPackage.getExploitTargets().getExploitTargets().get(0);	
+			ExploitTarget etExpected = (ExploitTarget)expectedPackage.getExploitTargets().getExploitTargets().get(0);	
+					
+			VulnerabilityType vulnReceived = etReceived.getVulnerabilities().get(0);
+			VulnerabilityType vulnExpected = etExpected.getVulnerabilities().get(0);
+		
+			assertEquals(vulnReceived, vulnExpected);
+										
+
 		} catch (IOException e)	{
 			e.printStackTrace();
 		}
@@ -190,52 +248,60 @@ public class BugtraqToStixExtractorTest {
 			solution = pageContent.get("solution");
 			references = pageContent.get("references");
 			
-			//TODO maybe add a BugtraqExtractor(Map)?
-			BugtraqExtractor bugtraqExt = new BugtraqExtractor(info, discussion, exploit, solution, references);
-			JSONObject obj = bugtraqExt.getGraph();
-		    
-		    //System.out.println(obj.toString(2));
+			BugtraqToStixExtractor bugtraqExt = new BugtraqToStixExtractor(info, discussion, exploit, solution, references);
+			STIXPackage receivedPackage = bugtraqExt.getStixPackage();
+						
+		//	System.out.println(receivedPackage.toXMLString(true));		    
 
-		    JSONArray verts = obj.getJSONArray("vertices");
-		    JSONArray edges = obj.getJSONArray("edges");
-		    
-		    String expectedVerts = "[{"+
-		    		  "  'accessVector': 'REMOTE'," +
-		    		  "  'Credit': 'rgod'," +
-		    		  "  'class': 'Boundary Condition Error'," +
-		    		  "  'CVE': 'CVE-2015-2098'," +
-		    		  "  'solution': 'Solution: Currently, we are not aware of any vendor-supplied patches. If you feel we are in error or are aware of more recent information, please mail us at: vuldb@securityfocus.com.'," +
-		    		  "  'exploit': 'Currently, we are not aware of any working exploits. If you feel we are in error or if you are aware of more recent information, please mail us at: vuldb@securityfocus.com.'," +
-		    		  "  'modifiedDate': 1427414400000," +
-		    		  "  'vertexType': 'vulnerability'," +
-		    		  "  '_type': 'vertex'," +
-		    		  "  'references': ['http://support.microsoft.com/kb/240797'],"+
-		    		  "  '_id': 'Bugtraq_72838'," +
-		    		  "  'source': 'Bugtraq'," +
-		    		  "  'shortDescription': 'WebGate eDVR Manager ActiveX Controls CVE-2015-2098 Multiple Buffer Overflow Vulnerabilities'," +
-		    		  "  'description': \"WebGate eDVR Manager ActiveX Controls CVE-2015-2098 Multiple Buffer Overflow Vulnerabilities WebGate eDVR Manager is prone to multiple buffer-overflow vulnerabilities because it fails to perform boundary checks before copying user-supplied data to insufficiently sized memory buffer. The controls are identified by CLSID's: 359742AF-BF34-4379-A084-B7BF0E5F34B0 4E14C449-A61A-4BF7-8082-65A91298A6D8 5A216ADB-3009-4211-AB77-F1857A99482C An attacker can exploit these issues to execute arbitrary code in the context of the application, usually Internet Explorer, using the ActiveX control.Failed attacks will likely cause denial-of-service conditions.\"," +
-		    		  "  'name': 'Bugtraq ID 72838'," +
-		    		  "  'Vulnerable': []," +
-		    		  "  'Not_Vulnerable': []," +
-		    		  "  'publishedDate': 1427414400000" +
-			    	  "}]";
-		    String expectedEdges = "[]";
-		    
-		    //System.out.println("Vertex list was: \n" + verts);
-		    //System.out.println("Edge list was: \n" + edges);
+		     	String expectedVerts =
+				"<?xml version=\"1.0\" encoding=\"UTF-8\"?> " +
+				"<stix:STIX_Package " +
+				"    id=\"stucco:bugtraq-c203648b-dbe3-4ec1-a03f-b7f148335db9\" " +
+				"    timestamp=\"2015-07-09T21:53:30.235Z\" " +
+				"    xmlns:coa=\"http://stix.mitre.org/CourseOfAction-1\" " +
+				"    xmlns:et=\"http://stix.mitre.org/ExploitTarget-1\" " +
+				"    xmlns:stix=\"http://stix.mitre.org/stix-1\" " +
+				"    xmlns:stixCommon=\"http://stix.mitre.org/common-1\" xmlns:stucco=\"stucco\"> " +
+				"    <stix:STIX_Header> " +
+				"        <stix:Title>Bugtraq</stix:Title> " +
+				"    </stix:STIX_Header> " +
+				"    <stix:Exploit_Targets> " +
+				"        <stixCommon:Exploit_Target " +
+				"            id=\"stucco:bugtraq-08391dce-8fcf-4ef0-8081-189abf733c17\" " +
+				"            xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:type=\"et:ExploitTargetType\"> " +
+				"            <et:Vulnerability> " +
+				"                <et:Description>WebGate eDVR Manager ActiveX Controls CVE-2015-2098 Multiple Buffer Overflow Vulnerabilities WebGate eDVR Manager is prone to multiple buffer-overflow vulnerabilities because it fails to perform boundary checks before copying user-supplied data to insufficiently sized memory buffer. The controls are identified by CLSID's: 359742AF-BF34-4379-A084-B7BF0E5F34B0 4E14C449-A61A-4BF7-8082-65A91298A6D8 5A216ADB-3009-4211-AB77-F1857A99482C An attacker can exploit these issues to execute arbitrary code in the context of the application, usually Internet Explorer, using the ActiveX control.Failed attacks will likely cause denial-of-service conditions.</et:Description> " +
+				"                <et:Short_Description>WebGate eDVR Manager ActiveX Controls CVE-2015-2098 Multiple Buffer Overflow Vulnerabilities</et:Short_Description> " +
+				"                <et:CVE_ID>CVE-2015-2098</et:CVE_ID> " +
+				"                <et:Source>Bugtraq</et:Source> " +
+				"                <et:Discovered_DateTime>2015-03-26T20:00:00.000-04:00</et:Discovered_DateTime> " +
+				"                <et:References> " +
+				"                    <stixCommon:Reference>http://support.microsoft.com/kb/240797</stixCommon:Reference> " +
+				"                </et:References> " +
+				"            </et:Vulnerability> " +
+				"            <et:Potential_COAs> " +
+				"                <et:Potential_COA> " +
+				"                    <stixCommon:Course_Of_Action xsi:type=\"coa:CourseOfActionType\"> " +
+				"                        <coa:Description>Solution: Currently, we are not aware of any vendor-supplied patches. If you feel we are in error or are aware of more recent information, please mail us at: vuldb@securityfocus.com.</coa:Description> " +
+				"                    </stixCommon:Course_Of_Action> " +
+				"                </et:Potential_COA> " +
+				"            </et:Potential_COAs> " +
+				"        </stixCommon:Exploit_Target> " +
+				"    </stix:Exploit_Targets> " +
+				"</stix:STIX_Package> ";
+								
+			STIXPackage expectedPackage = new STIXPackage().fromXMLString(expectedVerts);
+										
+			assertTrue(bugtraqExt.validate(receivedPackage));
+		
+			ExploitTarget etReceived = (ExploitTarget)receivedPackage.getExploitTargets().getExploitTargets().get(0);	
+			ExploitTarget etExpected = (ExploitTarget)expectedPackage.getExploitTargets().getExploitTargets().get(0);	
+					
+			VulnerabilityType vulnReceived = etReceived.getVulnerabilities().get(0);
+			VulnerabilityType vulnExpected = etExpected.getVulnerabilities().get(0);
+		
+			assertEquals(vulnReceived, vulnExpected);
 
-		    boolean match = HTMLExtractor.deepCompareJSONArraysUnordered(verts, new JSONArray(expectedVerts));
-		    if(!match){
-		    	System.out.println("Vertex list did not match!  result was: \n" + verts);
-		    }
-		    assertTrue( match );
-		    match = HTMLExtractor.deepCompareJSONArraysUnordered(edges, new JSONArray(expectedEdges));
-		    if(!match){
-		    	System.out.println("Edge list did not match!  result was: \n" + edges);
-		    }
-		    assertTrue( match );
-		    
-		    
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail("IOException");
